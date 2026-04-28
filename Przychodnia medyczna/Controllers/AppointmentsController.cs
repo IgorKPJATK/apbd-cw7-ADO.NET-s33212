5653f32c88d2daf4b1e8883c0c419704786516b0
Igor Kozuchowski <s33212@pjwstk.edu.pl>
2026-04-28 23:13:46 +0200

    tworzenie appointmensController

---
+++ Przychodnia medyczna/Controllers/AppointmentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Przychodnia_medyczna.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AppointmentsController :ControllerBase
+{
+    private readonly string _connectionString;
+
+    public AppointmentsController(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection") 
+                            ?? throw new InvalidOperationException("Connection string not found.");
+    }
+}
